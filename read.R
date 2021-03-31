b35737209db2c0d0d40542f54b94650668786d18
Kiyeon Lee <16848778+kiyeon7@users.noreply.github.com>
2021-03-31 17:10:43 -0400

    updates

--- read.R
+++ read.R
@@ -4,21 +4,37 @@ library(readxl)
 library(tidyverse)
 library(ipumsr)
 library(rstanarm)
+library(sjlabelled)
+
+
+class(sample$IND)[1]
+
+# returns: "haven_labelled"
+
+haven <- sample %>%
+  select(IND) %>%
+  mutate(ind = as_factor(IND))
+
+class(sample$IND)
+
+# returns: "factor"
+
+
+
 
 # Read in data
 
 if (!require("ipumsr")) stop("Reading IPUMS data into R requires the ipumsr package. 
                              It can be installed using the following command: install.packages('ipumsr')")
 
-ddi <- read_ipums_ddi("raw_data/usa_00002.xml")
-raw_data <- read_ipums_micro(ddi)
+ddi <- read_ipums_ddi("raw_data_3/usa_00003.xml")
+x <- read_ipums_micro(ddi)
 
 # Save file locally. Comment out afterwards.
+saveRDS(data, "raw_data_3/anyname.rds")
+rds_data <- read_rds("raw_data_3/anyname.rds")
 
-# saveRDS(data, "raw_data/anyname.rds")
-# rds_data <- read_rds("raw_data/anyname.rds")
 
 # Use smaller sample for faster loading
-
-# small <- x %>% 
-#   slice_sample(n = 1000)
+small <- x %>%
+  slice_sample(n = 1000)
